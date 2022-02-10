c802c0ad1a1ca2f6a83ff53921e95f519b07f2cd
sakeebadnan <47377110+sakeebadnan@users.noreply.github.com>
2022-02-10 14:02:34 +0100

    2nd
    sec

--- src/conn_data.php
+++ src/conn_data.php
@@ -1,9 +1,10 @@
 <?php
     // Local db
-    $server = 'localhost';
-    $dbName = 'fbdb';
-    $user = 'root';
-    $pwd = '';
+    $server = 'esilxl0nthgloe1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306';
+    $dbName = 'itjzmowwfdbsy6jd';
+    $user = 'pgaoqqxwoe1gfn3i';
+    $pwd = 'f1osbl7i2v99gyt2';
+    
 
 
 ?>
